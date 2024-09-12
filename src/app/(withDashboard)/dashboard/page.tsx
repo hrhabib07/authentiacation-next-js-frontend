f9d495e4bc0899ea9613561e4e8c0ff22bfac399
MD HABIBUR RAHMAN <mdhabibur.hr7@gmail.com>
2024-09-12 08:39:47 +0600

    user data diplayed and navbar session data passed

--- src/app/(withDashboard)/dashboard/page.tsx
+++ src/app/(withDashboard)/dashboard/page.tsx
@@ -1,7 +1,32 @@
-const DashboardPage = () => {
+import { authOptions } from "@/utlis/authOption";
+import { getServerSession } from "next-auth";
+import Image from "next/image";
+
+const DashboardPage = async () => {
+  const session = await getServerSession(authOptions);
+  console.log(session);
+
   return (
     <div>
-      <h1 className="text-4xl text-center mt-10">Welcome To Dashboard Page</h1>
+      {session?.user && (
+        <>
+          <h1 className="text-4xl text-center mt-10">
+            Welcome {session?.user.name}
+          </h1>
+          <p className="text-xl text-center mt-10">
+            Logged-in user Email: {session?.user.email}
+          </p>
+          <div>
+            <Image
+              src={session?.user.image as string}
+              width={200}
+              height={200}
+              className="mx-auto my-5 rounded-full border-2 border-gray-600"
+              alt="user image"
+            ></Image>
+          </div>
+        </>
+      )}
     </div>
   );
 };
